8e388728177ba80065c8bec9b97eeba8d5565a88
TheoToni <32997269+TheoToni@users.noreply.github.com>
2023-04-10 16:48:47 +0200

    Implementing Healthbar

--- Character.js
+++ Character.js
@@ -1,7 +1,11 @@
 import { getDiceRollArray, getDicePlaceholderHtml } from "./utils.js";
 
+const getPercentage = (remainingHealth, maximumHealth) =>
+  (100 * remainingHealth) / maximumHealth;
+
 function Character(data) {
   Object.assign(this, data);
+  this.maxHealth = this.health;
 
   this.diceArray = getDicePlaceholderHtml(this.diceCount);
 
@@ -23,9 +27,14 @@ function Character(data) {
     }
   };
 
+  this.getHealthBarHtml = function () {
+    const percent = getPercentage(this.health, this.maxHealth);
+    console.log(percent);
+  };
+
   this.getCharacterHtml = function () {
     const { elementId, name, avatar, health, diceCount } = this;
-
+    const healthBar = this.getHealthBarHtml();
     return `
             <div class="character-card">
                 <h4 class="name"> ${name} </h4>
